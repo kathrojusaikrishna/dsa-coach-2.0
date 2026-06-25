b53f2a807777884e5a9a287728db587d49cf525a
Kathroju Sai Krishna <kathsaikrishnaper@gmail.com>
2026-06-26 00:46:06 +0530

    Fix Redis port configuration

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -5,7 +5,7 @@ const redisClient = createClient({
   password: process.env.REDIS_PASSWORD,
   socket: {
     host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
+    port: process.env.REDIS_PORT,
   },
 });
 
